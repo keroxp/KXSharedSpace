54e24e1616583774093a286f4882039ca636599d
Yusuke Sakurai <kerokerokerop@gmail.com>
2014-02-03 12:05:07 +0900

    Dynamic KVOのクラスを作成

---
+++ Classes/KXDynamicKVO.h
@@ -0,0 +1,14 @@
+//
+//  KXDynamicKVO.h
+//  KXSharedSpace
+//
+//  Created by 桜井雄介 on 2014/02/02.
+//  Copyright (c) 2014年 Yusuke Sakurai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSObject+KXSharedSpace.h"
+
+@interface KXDynamicKVO : NSProxy
+
+@end
