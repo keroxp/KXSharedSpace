bbd865f7dd42ec1472cc17a00b74f4637a21e9b0
Yusuke Sakurai <kerokerokerop@gmail.com>
2014-03-12 14:56:00 +0900

    テスト用のデモを追加

---
+++ KXSharedSpace/KXSpacesDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  KXSpacesDetailViewController.h
+//  KXSharedSpace
+//
+//  Created by 桜井雄介 on 2014/03/12.
+//  Copyright (c) 2014年 Yusuke Sakurai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KXSpacesDetailViewController : UITableViewController
+
+@property (nonatomic) NSString *spaceKey;
+
+@end
---
+++ KXSharedSpace/KXSpacesDetailViewController.m
@@ -0,0 +1,160 @@
+//
+//  KXSpacesDetailViewController.m
+//  KXSharedSpace
+//
+//  Created by 桜井雄介 on 2014/03/12.
+//  Copyright (c) 2014年 Yusuke Sakurai. All rights reserved.
+//
+
+#import "KXSpacesDetailViewController.h"
+#import "KXSharedSpace.h"
+
+@interface KXSpacesDetailViewController ()
+
+@end
+
+@implementation KXSpacesDetailViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.tableView reloadData];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 2;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:
+            return @"Owners";
+            break;
+        case 1:
+            return @"Values";
+        default:
+            break;
+    }
+    return nil;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    switch (section) {
+        case 0:
+            return [[[[KXSharedSpace sharedSpace] spaceWithName:self.spaceKey] owners] count];
+        case 1:
+            return [[[[[KXSharedSpace sharedSpace] spaceWithName:self.spaceKey] dictionary] allKeys] count];
+        default:
+            break;
+    }
+    return 0;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    switch (indexPath.section) {
+        case 0: {
+            cell.textLabel.text = [[[[[[KXSharedSpace sharedSpace] spaceWithName:self.spaceKey] owners] allObjects] objectAtIndex:indexPath.row] description];
+        }
+            break;
+        case 1: {
+            NSString *s = [[[[[KXSharedSpace sharedSpace] spaceWithName:self.spaceKey] dictionary] allKeys] objectAtIndex:indexPath.row];
+            cell.textLabel.text = s;
+            cell.detailTextLabel.text = [[[[[KXSharedSpace sharedSpace] spaceWithName:self.spaceKey] dictionary] objectForKey:s] description];
+        }
+            break;
+        default:
+            break;
+    }
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
